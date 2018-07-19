7b359d71ad82b29ecf4f4b471aa70553eb2c66d3
Daniel Baumert <DBaumertffg@web.de>
2018-07-19 07:58:54 +0200

    Add files via upload

---
+++ scss/js/main.jsx
@@ -0,0 +1,15 @@
+const items = [
+ "itemA",
+ "itemB",
+  "itemC"
+]
+
+//=============================================
+ReactDOM.render(
+    <div style={{ display: "inline-flex" }}>
+        <DropDown title={items[0]} items={items} readOnly toggeled />
+        <DropDown title={items[1]} items={items} toggeled />
+        <DropDown title="custom" items={items} toggeled />
+    </div>
+   ,document.getElementById('root')
+);  
